587740e9f904e323771f06023ad48ecca888e120
Micah Silverman <micah.silverman@didit.com>
2011-01-30 16:39:16 -0500

    Properly nested calls so that async functions. Added calls to location services.

--- Resources/app.js
+++ Resources/app.js
@@ -18,82 +18,108 @@ var tab1 = Titanium.UI.createTab({
     window:win1
 });
 
-var myCoords = {
-  latitude: 0,
-  longitude: 0,
-  altitude: 0,
-  accuracy: 0,
-  altitudeAccuracy: 0,
-  heading: 0,
-  speed: 0,
-  timestamp: 0
-};
+Titanium.Geolocation.purpose = "Sample";
+Titanium.Geolocation.getCurrentPosition(function(e) {
+  if (e.error) { Ti.API.error('geo - current position' + e.error); return; }
+  var latitude = e.coords.latitude;
+  var longitude = e.coords.longitude;
 
-Ti.Geolocation.purpose = "Sample";
-Ti.Geolocation.getCurrentPosition(
-  function(e) {
-    if (e.error) { Ti.API.error('geo - current position' + e.error); return; }
-    myCoords.latitude = e.coords.latitude;
-    myCoords.longitude = e.coords.longitude;
-    myCoords.altitude = e.coords.altitude;
-    myCoords.accuracy = e.coords.accuracy;
-    altitudeAccuracy = e.coords.altitudeAccuracy;
-    myCoords.heading = e.coords.heading;
-    myCoords.speed = e.coords.speed;
-    myCoords.timestamp = e.coords.timestamp;
-    Ti.API.info('geo - current position');
-    Ti.API.info(' - latitude: ' + myCoords.latitude);
-    Ti.API.info(' - longitude: ' + myCoords.longitude);
-    Ti.API.info(' - altitude: ' + myCoords.altitude);
-    Ti.API.info(' - accuracy: ' + myCoords.accuracy);
-    Ti.API.info(' - altitudeAccuracy: ' + myCoords.altitudeAccuracy);
-    Ti.API.info(' - heading: ' + myCoords.heading);
-    Ti.API.info(' - speed: ' + myCoords.speed);
-    Ti.API.info(' - timestamp: ' + myCoords.timestamp);
-  }
-);
+  Ti.API.info('geo - current position');
+  Ti.API.info(' - latitude: ' + latitude);
+  Ti.API.info(' - longitude: ' + longitude);
 
-var mapView = Ti.Map.createView({
-  mapType: Ti.Map.STANDARD_TYPE,
-  region: {latitude: myCoords.latitude, longitude: myCoords.longitude, latitudeDelta:0.5, longitudeDelta:0.5},
-  animate:true,
-  regionFit:true,
-  userLocation:true
+  //send my coords
+  var xhr = Ti.Network.createHTTPClient();
+  xhr.onload = function(e) {
+    var responseData = JSON.parse(this.responseText);
+    Ti.API.info('Response data: ' + responseData.result + ": " + responseData.message);
+
+    // Get Rob's Coords
+    var robLatitude = 0;
+    var robLongitude = 0;
+    var xhr1 = Ti.Network.createHTTPClient();
+    xhr1.onload = function(e) {
+      var responseData = JSON.parse(this.responseText);
+      Ti.API.info('Response data: ' + responseData.result + ": " + responseData.message);
+
+      robLatitude = parseFloat(responseData.latitude);
+      robLongitude = parseFloat(responseData.longitude);
+
+      var robLocation = Titanium.Map.createAnnotation({
+          latitude: robLatitude,
+          longitude: robLongitude,
+          title: "Rob's Location",
+          subtitle: '',
+          pincolor:Titanium.Map.ANNOTATION_RED,
+          animate:true,
+          leftButton: '../images/appcelerator_small.png',
+          myid:1 // CUSTOM ATTRIBUTE THAT IS PASSED INTO EVENT OBJECTS
+      });
+
+      var micahLocation = Titanium.Map.createAnnotation({
+          latitude: latitude,
+          longitude: longitude,
+          title: "Micah's Location",
+          subtitle: '',
+          pincolor:Titanium.Map.ANNOTATION_GREEN,
+          animate:true,
+          leftButton: '../images/appcelerator_small.png',
+          myid:1 // CUSTOM ATTRIBUTE THAT IS PASSED INTO EVENT OBJECTS
+      });
+
+      var boston = {latitude: 42.334537, longitude: -71.170101, latitudeDelta: 0.0001, longitudeDelta: 0.0001};
+      var myLoc = {latitude: latitude, longitude: longitude, latitudeDelta: 1, longitudeDelta: 1};
+
+      var mapView = Titanium.Map.createView({
+        mapType: Titanium.Map.STANDARD_TYPE,
+        region: myLoc,
+        animate: true,
+        regionFit: true,
+        userLocation: true,
+        annotations: [robLocation, micahLocation]
+      });
+
+      mapView.addEventListener('complete', function(evt) {
+        Ti.API.info('in complete listener...');
+      });
+
+      mapView.addEventListener('regionChanged', function(evt) {
+        Ti.API.info('in regoinChanged listener...');
+      });
+
+      win1.add(mapView);
+
+      var win2 = Titanium.UI.createWindow({
+          title:'Tab 2',
+          backgroundColor:'#fff'
+      });
+      var tab2 = Titanium.UI.createTab({
+          icon:'KS_nav_ui.png',
+          title:'Tab 2',
+          window:win2
+      });
+
+      var label2 = Titanium.UI.createLabel({
+        color:'#999',
+        text:'I am Window 2',
+        font:{fontSize:20,fontFamily:'Helvetica Neue'},
+        textAlign:'center',
+        width:'auto'
+      });
+
+      win2.add(label2);
+
+      tabGroup.addTab(tab1);
+      tabGroup.addTab(tab2);
+
+      tabGroup.open();
+
+    };
+    xhr1.open('POST','http://microb.mpowerit.com:4000/getLocation');
+    xhr1.send({ name: 'rob' });
+
+  };
+
+  xhr.open('POST','http://microb.mpowerit.com:4000/setLocation');
+  xhr.send({ name: 'micah', latitude: latitude, longitude: longitude });
 });
-
-win1.add(mapView);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-  color:'#999',
-  text:'I am Window 2',
-  font:{fontSize:20,fontFamily:'Helvetica Neue'},
-  textAlign:'center',
-  width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);
-tabGroup.addTab(tab2);
-
-
-// open tab group
-tabGroup.open();
